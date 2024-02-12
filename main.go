fadcc661ebc8341ec4c12b5f0db02d37137747f9
Aspandiyar933 <70935356+Aspandiyar933@users.noreply.github.com>
2024-02-12 16:49:23 +0000

    So now I have worked with own machine and curl doesn't work. Tomorrow I will try fix it.

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
+
 func main() {
 
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request){
+	http.HandleFunc("/", func(w http.ResponseWriter, r*http.Request){
 		log.Println("Hello, World")
+		d, _ := io.ReadAll(r.Body)
+		fmt.Fprintf(w, "Hello, %s", d)
 	})
 
 	http.HandleFunc("/aspan", func(http.ResponseWriter, *http.Request){
